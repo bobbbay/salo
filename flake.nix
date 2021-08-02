1d75aaec8e194aec39b21af16ef5c5c39d3c01f0
Bobbbay <abatterysingle@gmail.com>
2021-08-02 19:32:52 -0400

    [general] Add project as package to Flake

--- flake.nix
+++ flake.nix
@@ -5,17 +5,27 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    naersk.url = "github:nix-community/naersk";
+
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, fenix, flake-utils, nixpkgs }:
+  outputs = { self, fenix, naersk, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (
       system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
         in
           {
+            packages.salo = (
+              naersk.lib.${system}.override {
+                inherit (fenix.packages.${system}.minimal) cargo rustc;
+              }
+            ).buildPackage { src = ./salo; };
+
+            defaultPackage = self.packages.${system}.salo;
+
             devShell = pkgs.mkShell {
               buildInputs = with pkgs; [ cargo-watch ];
             };
