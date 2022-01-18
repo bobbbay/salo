4c07fcb7f0139c2ee4598ae9d0c5ecf7731e8e87
bobbbay <abatterysingle@gmail.com>
2022-01-17 20:38:29 -0500

    feature(app): create command line parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,27 @@
 module Main where
 
+import Options.Applicative
+import Data.Monoid
+import Data.List
+
 main :: IO ()
-main = do
-  putStrLn "Hello, Haskell!"
+main = execParser opts >>= run
+
+data Sample
+  = Repl
+  deriving (Eq, Show)
+
+sample :: Parser Sample
+sample = subparser
+       ( command "repl"
+         (info (pure Repl)
+               (progDesc "Open the Salo REPL"))
+       )
+
+run :: Sample -> IO ()
+run Repl = putStrLn "Hello, world!"
+
+opts :: ParserInfo Sample
+opts = info (sample <**> helper)
+  ( fullDesc
+  <> header "salo - a toolset to agnostically build and deploy OS images remotely" )
