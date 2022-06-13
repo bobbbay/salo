8d09c1b2f460cd33c27c1f055c16708c7bcaedf7
Bobbbay <abatterysingle@gmail.com>
2022-06-13 21:50:36 +0000

    chore(nix): update flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,26 @@
 {
+  description = "Declarative, reproducible, statically typed build and deployment for Operating Systems.";
+
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
     nci.url = "github:yusdacra/nix-cargo-integration";
+    nci.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs: inputs.nci.lib.makeOutputs { root = ./.; };
+
+  outputs = { self, nixpkgs, nci, ... }:
+    nci.lib.makeOutputs {
+      root = ./.;
+      overrides = {
+        shell = common: prev: {
+          packages = prev.packages ++ (with common.pkgs; [
+            rust-analyzer
+            cargo-watch
+          ]);
+        };
+      };
+    };
 }
